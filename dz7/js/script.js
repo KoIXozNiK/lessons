8cafce636f4ec107dec380cac2f6b761dfba23a3
KoIXozNiK <vasylyk29@gmail.com>
2019-01-03 00:16:52 +0200

    fix

--- dz7/js/script.js
+++ dz7/js/script.js
@@ -1,4 +1,3 @@
-console.log('load');
 window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
@@ -12,6 +11,7 @@ window.addEventListener('DOMContentLoaded', function () {
             tabContent[i].classList.add('hide');
         }
     }
+    hideTabContent(1);
 
     function showTabContent(b){
         if ( tabContent[b].classList.contains('hide')) {
