78a13a8e32411698c5242d4bba9e131638d9bb98
KoIXozNiK <vasylyk29@gmail.com>
2019-01-08 23:08:21 +0200

    timer

--- dz7/js/script.js
+++ dz7/js/script.js
@@ -1,30 +1,32 @@
 window.addEventListener('DOMContentLoaded', function () {
 
+    // TABS
+
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a){
-        for( let i = a; i < tabContent.length; i++ ) {
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
     hideTabContent(1);
 
-    function showTabContent(b){
-        if ( tabContent[b].classList.contains('hide')) {
-             tabContent[b].classList.remove('hide');
-             tabContent[b].classList.add('show');
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function(e){
+    info.addEventListener('click', function (e) {
         let target = e.target;
-        if ( target && target.classList.contains('info-header-tab')) {
-            for ( let i = 0; i < tab.length; i++ ) {
-                if ( target == tab[i]){
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
                     break;
@@ -32,4 +34,45 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+
+
+    //TIMER
+    let deadline = '2019-03-22';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 * 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            "total": t,
+            "second": seconds,
+            "minutes": minutes,
+            "hours": hours,
+        };
+    }
+
+    function setCloak(id, endtime) {
+        let timer = document.getElementById('timer'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(undateCloack, 1000);
+
+
+        function undateCloack() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.second;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setCloak('timer', deadline)
+
+
 });
