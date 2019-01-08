a67156e941210c1b1dd7292099753eb085e1aca5
KoIXozNiK <vasylyk29@gmail.com>
2019-01-08 23:17:45 +0200

    fix

--- dz7/js/script.js
+++ dz7/js/script.js
@@ -1,8 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+
 
     // TABS
-
-    'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
