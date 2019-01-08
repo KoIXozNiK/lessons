7c0e04e1c825b1f8cc63f54437bafefcda0dd5cc
KoIXozNiK <vasylyk29@gmail.com>
2019-01-08 23:44:45 +0200

    update

--- dz7/js/script.js
+++ dz7/js/script.js
@@ -38,12 +38,12 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
     //TIMER
-    let deadline = '2019-03-22';
+    let deadline = '2019-01-10';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t / 1000) % 60),
-            minutes = Math.floor((t / 1000 * 60) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
             hours = Math.floor((t / (1000 * 60 * 60)));
 
         return {
@@ -55,7 +55,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     function setCloak(id, endtime) {
-        let timer = document.getElementById('timer'),
+        let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
@@ -64,12 +64,22 @@ window.addEventListener('DOMContentLoaded', function () {
 
         function undateCloack() {
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.second;
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return "0" + num;
+                } else return num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.second);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }
     }
